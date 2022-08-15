1549ad2b75f6c65a04f3cbd856f419cab9d07089
quanghuy <huynguyenquang@tcom.vn>
2022-08-15 08:41:27 +0700

    update

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
             arrows: false,
             dots: false,
             loop: false,
-            autoplay: true,
+            // autoplay: true,
             speed: 1000,
             autoplaySpeed: 3000,
             responsive: [
@@ -69,3 +69,41 @@ function anchorLink(element){
         }, 1000)
     });
 }
+
+
+var pmfFileupload = {
+    /* 
+       Pretty mother fuckin File Upload input
+    */
+    init: function() {
+       this.cacheDom();
+       this.events();
+    },
+    cacheDom: function() {
+       this.$filePlaceholder = $('.file-placeholder');
+       this.$filelabel = this.$filePlaceholder.find('label');
+       this.$fileUpload = this.$filePlaceholder.find('.fileUpload');
+       this.$fileBrowseTxt = this.$filePlaceholder.find('.file-browse-txt');
+    },
+    events: function() {
+       this.$fileUpload.on('change', this.getFileName.bind(this));
+    },
+    getFileName: function() {
+       this.newVal = this.$fileUpload.val();
+       if (this.newVal !== "") {
+          this.$fileBrowseTxt.text(this.newVal).addClass('hasValue');
+       } else {
+          this.$fileBrowseTxt.text("Select a file...");
+       }
+    }
+ };
+ 
+ $(document).ready(function() {
+    pmfFileupload.init();
+ });
+ 
+
+
+
+
+
